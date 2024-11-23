1d1c09b1709830da4961b827462c9ac74d999614
fiufiu <fiufiu@maneiro>
2024-11-22 21:00:57 -0300

    desafio atualizado

--- aula04/desafioAluno.js
+++ aula04/desafioAluno.js
@@ -1,4 +1,4 @@
-let prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")()
 
 let nota1 = prompt("Digite a primeira nota do aluno:")
 
@@ -10,10 +10,15 @@ let media = (Number(nota1) + Number(nota2) + Number(nota3)) / 3
 
 let falta = prompt("Digite a porcentagem de faltas do aluno (apenas o número):")
 
+var resultado
+
 if(media >= 7 && falta < 25){
-    console.log("Aluno aprovado!")
+    resultado = "Aluno aprovado!"
 } else if (media <7 && media >= 5 && falta <25){
-    console.log("Recuperação")
+    resultado = "Recuperação"
 } else if (media < 5 || falta >= 25){
-    console.log("Aluno reprovado!")
+    resulado = "Aluno reprovado!"
 }
+
+console.log(`O aluno teve a média ${media.toFixed(1)} e teve ${falta}% de falta.\n Situação: ${resultado}`)
+
